2f08175432460d30e75eaee19fa72a05947fa29b
Berkan <sukutberkan@gmail.com>
2023-09-14 00:55:57 +0300

    5.4

---
+++ WebApplication1/Controllers/WriterPanelController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager hm=new HeadingManager(new EFHeadingDal());
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        public ActionResult MyHeading(int id)
+        {
+            id = 2;
+            var values=hm.GetListByWriter(id);
+            return View(values);
+        }
+    }
+}
