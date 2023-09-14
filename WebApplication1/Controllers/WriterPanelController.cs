530b51f1f164603a5b0bfa51995fc370c41e94df
Berkan <sukutberkan@gmail.com>
2023-09-14 21:21:10 +0300

    6.0

--- WebApplication1/Controllers/WriterPanelController.cs
+++ WebApplication1/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,61 @@ namespace WebApplication1.Controllers
     public class WriterPanelController : Controller
     {
         HeadingManager hm=new HeadingManager(new EFHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         // GET: WriterPanel
         public ActionResult WriterProfile()
         {
             return View();
         }
-        public ActionResult MyHeading(int id)
+        public ActionResult MyHeading()
         {
-            id = 2;
-            var values=hm.GetListByWriter(id);
+          
+            var values=hm.GetListByWriter();
             return View(values);
         }
+        [HttpGet]
+        public ActionResult NewHeading()
+        { 
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 2;
+            p.HeadingStatus = true;
+            hm.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
+        }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+
     }
 }
